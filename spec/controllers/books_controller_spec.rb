730d87e4d7495145cf34c9789d5d1184a763e871
Jorge Mayo <jmayomartin@gmail.com>
2013-06-30 13:26:20 +0200

    little refactoring to BooksController spec

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe BooksController do
-
+  before :each do
+    #first we must mock the current_user
+    Reader.stub(:find_by_id).and_return(mock('reader'))
+    #mocking result objects
+    @fake_result = mock('book')
+    @fake_results = [mock('book1'),mock('book2')]
+  end
   describe 'adding books to the library' do
-    before :each do
-      #first we must mock the current_user
-      Reader.stub(:find_by_id).and_return(mock('reader'))
-      @fake_result = mock('book')
-    end
     it 'should call a model method that create a book' do  
       @fake_result.stub(:title)
       Book.should_receive(:create!).and_return(@fake_result)
@@ -15,13 +16,7 @@ describe BooksController do
     end
   end
   describe 'searching books on Google books' do
-    before :each do
-      #first we must mock the current_user
-      Reader.stub(:find_by_id).and_return(mock('reader'))
-      @fake_results = [mock('book1'),mock('book2')]
-    end
     it 'should call a model method that performs google book search' do
-      
       Book.should_receive(:find_in_gbooks).with("El Quijote").and_return(@fake_results)
       post :search_gbdb, {:search_terms => "El Quijote"}
     end
