17c72d349d27e4d1e448864b3e577d486912ad74
Jorge Mayo <jmayomartin@gmail.com>
2013-07-18 00:03:01 +0200

    foud top 5 books query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,16 @@ class HomeController < ApplicationController
   
   def index
   end
+  
+  def top_books
+    #@books = Book.all
+    debugger
+    @books = Book.connection.select_all("SELECT books.*, avg(reviews.stars) as avg_rating
+      FROM books 
+      INNER JOIN reviews ON books.id = reviews.book_id 
+      group by books.id
+      ORDER BY reviews.stars desc
+      LIMIT 5")
+  end
+  
 end
